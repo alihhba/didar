9d8bdd2e6fa51e91bbffaa1696f5b87c1860e76a
alirezaHabibolahi <161218485+alirezaHabibolahi@users.noreply.github.com>
2025-02-04 00:11:47 +0330

    update ticket

--- src/pages/message/NewMessagePage.jsx
+++ src/pages/message/NewMessagePage.jsx
@@ -5,27 +5,24 @@ import Icon from "@/components/icons/Icon.jsx";
 import icons from "@/lib/utils/icons.js";
 import CustomInput from "@/components/ui/CustomInput.jsx";
 import Button from "@/components/ui/Button.jsx";
-import {cn} from "@/lib/utils/index.jsx";
-import DateFormatter from "@/components/DateFormatter.jsx";
 import usePostNewTicket from "@/hooks/usePostNewTicket.jsx";
-import useGetInstructorById from "@/hooks/useGetInstructorById.jsx";
-
-
 
 
 const NewMessageText = () => {
     const {state} = useLocation();
     const {id} = useParams();
     const [messageValue, setMessageValue] = useState("");
+    const [titleValue, setTitleValue] = useState("");
+
     const navigate = useNavigate();
-    const {postTicket:postNewTicket} = usePostNewTicket();
-    console.log(    state , 'state')
+    const {postTicket: postNewTicket} = usePostNewTicket();
+
 
     const handleSubmit = async () => {
         try {
 
-                const payload = { message: messageValue  , instructor:+id , title: messageValue };
-                await postNewTicket(payload);
+            const payload = {message: messageValue, instructor: +id, title: titleValue};
+            await postNewTicket(payload);
 
             setMessageValue('');
             navigate('/messages')
@@ -42,7 +39,7 @@ const NewMessageText = () => {
                 <div className={'flex items-center gap-2'}>
                     <div>
                         <Icon
-                            onClick={()=>{
+                            onClick={() => {
                                 navigate(-1)
                             }}
                             icon={icons.chevron_left} className={'w-2 h-2 rotate-180'}/>
@@ -60,26 +57,34 @@ const NewMessageText = () => {
 
             <Page.Content className={'px-4'}>
                 <div className={'w-full h-full flex flex-col grow gap-y-6'}>
-
-
-                    {/*input box*/}
-                    <div className={'w-full mt-auto fixed  bottom-24 px-2 right-0 left-0'}>
+                    <div className={'space-y-2'}>
                         <CustomInput
                             className={'h-[60px]'}
-                            placeholder={'نوشتن پیام'}
+                            placeholder={'موضوع'}
+                            value={titleValue}
+                            onChange={(e) => {
+                                setTitleValue(e.target.value);
+                            }}
+                        />
+                        <CustomInput
+                            className={'h-[60px]'}
+                            placeholder={' پیام'}
                             value={messageValue}
                             onChange={(e) => {
                                 setMessageValue(e.target.value);
                             }}
                         />
+                    </div>
 
-                        <div className={'absolute end-2 top-0.5 me-2'}>
+
+                    <div className={'w-full mt-auto fixed  bottom-24 px-2 right-0 left-0'}>
+                        <div className={'w-full'}>
                             <div className={'w-full py-2'}>
                                 <Button
                                     onClick={handleSubmit}
                                     style={'primary'}
-                                    className={'h-[44px] w-[44px] flex items-center justify-center'}>
-                                    <Icon icon={icons.send} className={'w-5 h-5'}/>
+                                    className={'h-[44px] w-full flex items-center justify-center'}>
+                                    ارسال
                                 </Button>
                             </div>
                         </div>
